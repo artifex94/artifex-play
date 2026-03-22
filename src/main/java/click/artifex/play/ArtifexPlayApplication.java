29977f1d9c80dd228ad798fb79d1de81e31c9827
artifex <dev@artifex.click>
2026-03-21 23:42:28 -0300

    Inicialización del proyecto artifex-play

---
+++ src/main/java/click/artifex/play/ArtifexPlayApplication.java
@@ -0,0 +1,13 @@
+package click.artifex.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifexPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifexPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/click/artifex/play/ArtifexPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package click.artifex.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifexPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
